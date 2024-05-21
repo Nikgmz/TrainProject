741ee233f7dcb23d779e7f4d502ceb92e2f65bec
Nikgmz <132126169+Nikgmz@users.noreply.github.com>
2024-05-21 08:51:34 +0300

    Added commments for WindowMain

--- src/WindowMain.java
+++ src/WindowMain.java
@@ -1,23 +1,23 @@
 import javax.swing.*;
 import java.awt.*;
 
-public class WindowMain extends JFrame {
+public class WindowMain extends JFrame {//towa tuka definira klasa kato naslednik na Jframe i shte e prozorec
 
 
     public WindowMain() throws HeadlessException {
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setLayout(new FlowLayout());
-        setSize(500, 500);
-        setTitle("Train Service App");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);//kato se zatwori prozoreca da se exitne
+        setLayout(new FlowLayout());//towa definira kak shte ni izglezhda GUI
+        setSize(500, 500);//nachalna golemina
+        setTitle("Train Service App");//zaglawie
         JButton goToSearch = new JButton("Go and search for routes!");
-        goToSearch.addActionListener(e -> {
-            SearchWindow searchWindow = new SearchWindow();
-            dispose();
+        goToSearch.addActionListener(e -> {//towa se prawi i bez lambda ama warninga kaza da go smenq
+            SearchWindow searchWindow = new SearchWindow();//otwarqme now prozorec
+            dispose();//zatwarqme prozoreca
         });
         add(goToSearch);
         
        JButton goApplyJob = new JButton("Go and apply for a job!");
-        goApplyJob.addActionListener(e->{
+        goApplyJob.addActionListener(e->{//analogichno na predischnoto
             JobApplication jobApplication = new JobApplication();
             dispose();
         });
